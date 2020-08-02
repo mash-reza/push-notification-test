0025328c7f88fca898a2b0175829b136dffb37df
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-02 23:09:46 +0430

    simple example

--- app/src/main/java/com/festive/pushnotificationtest/MainActivity.kt
+++ app/src/main/java/com/festive/pushnotificationtest/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.festive.pushnotificationtest
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+    private lateinit var TEXT: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start.setOnClickListener {
-            startService(Intent(this, MyService::class.java))
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                getResult1FromApi()
+                withContext(Dispatchers.Main){
+                    textView.text = TEXT
+                }
+            }
         }
 
-        stop.setOnClickListener {
-            stopService(Intent(this, MyService::class.java))
-        }
+    }
 
+    private suspend fun getResult1FromApi() {
+        delay(2000)
+        TEXT = "Result 1"
+        logThread("getResult1FromApi")
+    }
+
+    private suspend fun logThread(methodName: String) {
+        Log.d(TAG, "method name: $methodName in thread: ${Thread.currentThread().name}")
     }
 }
